a3f2c5e35a681518f1b3df841163c35d3d4a178c
Cong <ccong2@illinois.edu>
2019-07-01 15:51:21 +0000

    create a draft script of github api

--- src/03_github-repositories.R
+++ src/03_github-repositories.R
@@ -1,40 +1,106 @@
+#devtools::install_github("ropensci/ghql")
+library("ghql")
+library("jsonlite")
+library("httr")
+
+token <- '963108f94c445eb2e50587013c0356bfce308ff1'
+cli <- GraphqlClient$new(
+  url = "https://api.github.com/graphql",
+  headers = add_headers(Authorization = paste0("Bearer ", token))
+)
+
+cli$load_schema()
+qry <- Query$new()
+
+qry$query('getmydata','{
+          rateLimit {
+          cost
+          remaining
+          resetAt
+          }
+          search(query: "license:mit", type: REPOSITORY,first:100,after:"Y3Vyc29yOjEwMA==") {
+          repositoryCount
+          pageInfo {
+          endCursor
+          startCursor
+          hasNextPage
+          }
+          edges {
+          node {
+          ... on Repository {
+          owner {
+          login
+          }
+          name
+          }
+          }
+          }
+          }
+          }')
+
+
+resp <- cli$exec(qry$queries$getmydata)
+
+fj <- jsonlite::fromJSON(resp)
+
+fj$data$search$edges$node$name
+
+
+fj$data$search$pageInfo$endCursor
+
+while (fj$data$repository$issues$pageInfo$hasNextPage) {
+
+  qry$queries$getissues$query <- sprintf(getIssues, cursor)
+  q <- cli$exec(qry$queries$getissues)
+  cursor <- q$data$repository$issues$pageInfo$endCursor
+  issues <- c(issues, q$data$repository$issues$edges$node$number)
+}
+
+
+
 
 ##============================
 
-library(httr)
+url="https://api.github.com/graphql"
 
-auth_header <- paste('bearer', 'fc735803fd147b3562271439a1863957b5611496')
+auth_header <- paste("bearer", '963108f94c445eb2e50587013c0356bfce308ff1')
+agent="ccong2"
 
-test<-httr::POST(url="https://api.github.com/graphql", 
-                 add_headers(Authorization = auth_header),
-                 encode="json",
-                 body='{
-                   rateLimit {
-                     cost
-                     remaining
-                     resetAt
-                   }
-                   search(query: "license:mit", type: REPOSITORY, first: 100) {
-                     repositoryCount
-                     pageInfo {
-                       endCursor
-                       startCursor
-                     }
-                     edges {
-                       node {
-                         ... on Repository {
-                           owner {
-                             login
-                           }
-                           name
-                         }
-                       }
-                     }
-                   }
-                 }')
+pbody <-  'query{
+  rateLimit {
+    cost
+    remaining
+    resetAt
+  }
+  search(query: "license:mit", type: REPOSITORY, first: 20) {
+    repositoryCount
+    pageInfo {
+      endCursor
+      startCursor
+    }
+    edges {
+      node {
+        ... on Repository {
+          owner {
+            login
+          }
+          name
+        }
+      }
+    }
+  }
+}'
 
 
-res <- content(test, as = "parsed", encoding = "UTF-8")
+res <- POST(url, body = rjson::toJSON(pbody),  
+            add_headers(Authorization=auth_header,`User-Agent` = agent))
+
+library(stringr)
+body<-str_remove_all(body,"\n")
+body<-rjson::toJSON(body)
+body<-str_remove_all(body,"")
+
+res <- content(res, as = "parsed", encoding = "UTF-8")
 
 
 
