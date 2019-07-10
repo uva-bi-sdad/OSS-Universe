d1116bc59c9ec009ab5fccd30f2ff3021911e362
CalvinIsch <calvin.isch@gmail.com>
2019-07-10 15:57:55 +0000

    More work completed on the function to clean the data.

--- src/make_data_pretty.R
+++ src/make_data_pretty.R
@@ -7,37 +7,48 @@ queryResults2 <- as.data.table(result2$data[2])
 # The number of repos found in this query
 amountRepos <- queryResults2[[1,1]]
 
-# Gets the section of the DT that has more info form the query
-moreInfo <- as.data.table(queryResults2[[3,1]])
 
-# Gets the commit history for each repo
-commitHistory <- moreInfo$defaultBranchRef
+
+
 
 # Looks at the commits for the first repo
-firstRepoCommit <- (commitHistory[[2]][1,1])
+
 
 # Looks at the authors for that first repo
 View(firstRepoCommit$nodes[[1]]$author)
 
-# For every repository, gets all of the commits and binds them together into one 
+
+
+# Gets the section of the DT that has more info form the query
+moreInfo <- as.data.table(queryResults2[[3,1]])
+# Gets the commit history for each repo
+commitHistory <- moreInfo$defaultBranchRef
+
+# For every repository, gets all of the commits and binds them together into one data table
+# Also gets the authors of those commits and writes them into a table.
 for (i in 1:length(commitHistory[[2]]$history$nodes)) {
+  
   # Creates a data.table of the current repository's commit history
   currentRepo <- data.table(
     authorId = commitHistory[[2]]$history$nodes[[i]]$author$user$id,
     additions = commitHistory[[2]]$history$nodes[[i]]$additions,
     deletions = commitHistory[[2]]$history$nodes[[i]]$deletions
-    # We also want to get 
+    # We also want to get 1. Repo Name 2. Date
   )
+  
+  currentAuthor <- data.table(
+    email = commitHistory[[2]]$history$nodes[[i]]$author$email,
+    id = commitHistory[[2]]$history$nodes[[i]]$author$user$id
+    # Also want 1. Organization 2. Login 3. Company
+  )
+  
   # Binds the current repo's commit history to the list of existing commit history
-  if (exists("allRepos")) {
-    allRepos <- rbindlist(list(currentRepo, allRepos))
+  if (exists("allCommits")) {
+    allCommits <- rbindlist(list(currentRepo, allCommits))
+    allAuthors <- rbindlist(list(currentAuthor,allAuthors))
   } else {
-    allRepos <- currentRepo
+    allCommits <- currentRepo
+    allAuthors <- currentAuthor
   }
 }
-
-# Creates a data.table of the authors
-author<- data.table(
-  email = firstRepoCommit$nodes[[1]]$author$email,
-  id = firstRepoCommit$nodes[[1]]$author$user$id
-)
+allAuthors <- unique(allAuthors)
