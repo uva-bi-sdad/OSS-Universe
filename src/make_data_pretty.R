7ef63a6cee59f3d7a8f30eccbbedc9177e0d8c08
CalvinIsch <calvin.isch@gmail.com>
2019-07-10 17:10:08 +0000

    Finished the function and made it run faster.

--- src/make_data_pretty.R
+++ src/make_data_pretty.R
@@ -16,53 +16,39 @@
 
 makePretty <- function(res,queryDate) {
   
-  # Save initial thing from Github as a data.table
-  result2 <- as.data.table(result)
-  
-  # Takes only the important values from the query, and turns it to datatable
-  queryResults2 <- as.data.table(result2$data[2])
-  
   # Creates a table with information about the query
   allQueries <- data.table(
     date = queryDate,
-    numRepos = queryResults2[[1,1]]
+    numRepos = res$data$search$repositoryCount
   )
-  
-  
-  # Gets the information necessary in order to get num commits for a repo
-  repoInfo <- as.data.table(result2$data[2])[[3,1]][[3]]
   
   # Creates a data table with the repository-specific information
   allRepos <- data.table(
-    repoName = as.data.table(result2$data[2])[[3,1]][[1]],
-    owner = as.data.table(result2$data[2])[[3,1]][[2]],
-    defBranch = repoInfo$name,
-    numCommits = repoInfo$target$history$totalCount,
+    repoName = result$data$search$nodes$name,
+    owner = res$data$search$nodes$owner$login,
+    defBranch = res$data$search$nodes$defaultBranchRef$name,
+    numCommits = res$data$search$nodes$defaultBranchRef$target$history$totalCount,
     date = queryDate
   )
   
   
-  # Gets the section of the DT that has more info form the query
-  moreInfo <- as.data.table(queryResults2[[3,1]])
-  # Gets the commit history for each repo
-  commitHistory <- moreInfo$defaultBranchRef
-  
   # For every repository, gets all of the commits and binds them together into one data table
   # Also gets the authors of those commits and writes them into a table.
-  for (i in 1:length(commitHistory[[2]]$history$nodes)) {
+  for (i in 1:length(res$data$search$nodes$defaultBranchRef$target$history$nodes)) {
     
     # Creates a data.table of the current repository's commit history
     currentRepo <- data.table(
-      authorId = commitHistory[[2]]$history$nodes[[i]]$author$user$id,
-      additions = commitHistory[[2]]$history$nodes[[i]]$additions,
-      deletions = commitHistory[[2]]$history$nodes[[i]]$deletions,
-      repoName = as.data.table(result2$data[2])[[3,1]][[1]]
+      authorId = res$data$search$nodes$defaultBranchRef$target$history$nodes[[i]]$author$user$id,
+      additions = res$data$search$nodes$defaultBranchRef$target$history$nodes[[i]]$additions,
+      deletions = res$data$search$nodes$defaultBranchRef$target$history$nodes[[i]]$deletions,
+      repoName = res$data$search$nodes$name[i]
       # We also want to get  2. Date
     )
     
+    ### YOU ARE HERE!!! ###
     currentAuthor <- data.table(
-      email = commitHistory[[2]]$history$nodes[[i]]$author$email,
-      authorId = commitHistory[[2]]$history$nodes[[i]]$author$user$id
+      email = res$data$search$nodes$defaultBranchRef$target$history$nodes[[i]]$author$email,
+      authorId = res$data$search$nodes$defaultBranchRef$target$history$nodes[[i]]$author$user$id
       # Also want 1. Organization 2. Login 3. Company
     )
     
