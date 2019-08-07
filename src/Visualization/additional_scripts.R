966478c033e31029a118332948a7e2669fdae4c8
Cong <ccong2@illinois.edu>
2019-08-07 14:57:22 +0000

    add additional scripts

--- src/Visualization/additional_scripts.R
+++ src/Visualization/additional_scripts.R
@@ -7,7 +7,6 @@ library(RPostgreSQL)
 library(data.table)
 library(ggplot2)
 library(tidyverse)
-library(treemap)
 
 # Setting root directory
 knitr::opts_knit$set(echo = TRUE,
@@ -16,23 +15,20 @@ knitr::opts_knit$set(echo = TRUE,
 data <- fread("./data/oss/final/Github/all_repos_commits.csv")
 
 ###Match locations to countries
+#We uses the country names in package "maps" to match with the locations. 
 locations <- unique(data$location) %>% data.frame()
-# Loading country data from package maps
+# Loading country data  
 data(world.cities)
 #Removing punctuation
 raw <- gsub("[[:punct:]\n]","",locations[,1])
 # Split data at word boundaries
 raw2 <- strsplit(raw, " ")
-# Match on country in world.countries
+# Match on country in world.countries. This takes a while.  
 CountryList_raw <- (lapply(raw2, function(x)x[which(toupper(x) %in% toupper(world.cities$country.etc))]))
 
 match <- do.call(rbind, lapply(CountryList_raw, as.data.frame))
 
 
 #####Number of average contributors per repo
-comm <- data %>%group_by(license)%>%summarize(commits=mean(Total_commit))%>%
-  arrange(desc(commits))%>% na.omit()
-colnames(comm) <-c("license","commits")
-comm <- data.frame(comm)
-comm$license <- as.factor(comm$license)
-comm$commits <- as.numeric(comm$commits)
+comm <- data %>% group_by(repo_slug, license) %>% summarize(commits=sum(commit_cnt))%>%na.omit()
+comm <- comm %>% group_by(license) %>% summarize(mean_per_repo=mean(commits))
